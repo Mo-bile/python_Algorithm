9f1208203e43d22397311fada9d374bf0e089e6d
Mo <jym2013@gmail.com>
2024-01-09 23:03:39 +0900

    type : feat 1. 새콤달콤 장사 memorization 방식 문제풀이 완료 but 다시풀기 필요

--- algorithm_paradaim/DynamicProgramming/12.max_profit_memo.py
+++ algorithm_paradaim/DynamicProgramming/12.max_profit_memo.py
@@ -10,18 +10,20 @@ def max_profit_memo(price_list, count, cache):
         # count 개를 팔때 저장된 최대 값
         return cache[count]
 
+    if count < len(price_list):
+        profit = price_list[count]
+    else:
+        profit = 0
+
     # cache 내에 존재하지 않는 경우
     # 반복문 목적 : 나누는 경우의 수 각 만들어서 더하기
-    for i in range(2, count):
-        quotient = count // i
-        remainder = round(count / i + 0.5)
-
-        cache[count] = (
-            max_profit_memo(price_list, quotient, cache) +
-            max_profit_memo(price_list, remainder, cache)
-        )
-
-    return cache[count]
+    for i in range(1, count // 2 + 1):
+        profit = max(profit,
+                     max_profit_memo(price_list, i, cache) +
+                     max_profit_memo(price_list, count - i, cache)
+                     )
+    cache[count] = profit
+    return profit
 
 
 def max_profit(price_list, count):
@@ -32,5 +34,5 @@ def max_profit(price_list, count):
 
 # 테스트 코드
 print(max_profit([0, 100, 400, 800, 900, 1000], 5))
-# print(max_profit([0, 100, 400, 800, 900, 1000], 10))
-# print(max_profit([0, 100, 400, 800, 900, 1000, 1400, 1600, 2100, 2200], 9))
+print(max_profit([0, 100, 400, 800, 900, 1000], 10))
+print(max_profit([0, 100, 400, 800, 900, 1000, 1400, 1600, 2100, 2200], 9))
