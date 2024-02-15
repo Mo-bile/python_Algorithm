bf4f7d8090b0028cec9a7c50646eeaddf6c13e5f
Mo <jym2013@gmail.com>
2024-02-15 19:27:10 +0900

    type : feat 중급 알고리즘 Lv1-2 풀이 최종해답 보고 풀었음

--- dev_problem_soving_abilty/Level1/2_거듭제곱_빠르게_계산.py
+++ dev_problem_soving_abilty/Level1/2_거듭제곱_빠르게_계산.py
@@ -1,14 +1,17 @@
 def power(x, y):
+    x_table = list()
     #base case
     if  y < 1:
         return 1
 
-    #recursive case
-    x *= power(x , y - 2)
+    if y % 2 == 0 :
+        return power(x, y // 2) * power(x, y // 2)
+    else:
+        return x * power(x, y // 2) * power(x, y // 2)
 
-    return x
 
 # 테스트 코드
+# print(power(3, 4))
 print(power(3, 5))
-print(power(5, 6))
-print(power(7, 9))
+# print(power(5, 6))
+# print(power(7, 9))
