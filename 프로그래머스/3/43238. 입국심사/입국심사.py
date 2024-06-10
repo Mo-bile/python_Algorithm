23fe0d5f293894cbf49df50b34947289dfcf10e7
Mo <91646814+Mo-bile@users.noreply.github.com>
2024-06-10 23:21:41 +0900

    [level 3] Title: 입국심사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,17 +1,16 @@
 def solution(n, times):
     left, right = min(times), max(times) * n
     
-    
-    while left < right:
-        total = 0    
+    while left < right: # 이분탐색은 다음과 같이 종료 조건을 두어야함
+        sum = 0    
         mid = (left + right) // 2
         
         for time in times:
-            total += mid // time
+            sum += mid // time
         
-        if total >= n :
+        if sum >= n :
             right = mid
         else:
-            left = mid + 1
+            left = mid + 1 # mid 보다 + 1이 되게 해야함
 
-    return left
+    return left #left가 right 보다 크거나 같으면 left return
