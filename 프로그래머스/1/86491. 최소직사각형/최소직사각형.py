9bc3b6d9807ab2cf77c3c64c051257257f82ee19
Mo <91646814+Mo-bile@users.noreply.github.com>
2024-08-07 08:44:29 +0900

    [level 1] Title: 최소직사각형, Time: 4.21 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -2,17 +2,11 @@ def solution(sizes):
     big, small = [], []
     
     for size in sizes:
-        for i in range(len(size)):
-            big.append(max(size))
-            small.append(min(size))
-            
+        # for i in range(len(size)):
+        big.append(max(size))
+        small.append(min(size))
     
+    # 큰값과 작은 값 각각 도출하기
     bob = max(big)
-    
     sob = max(small)
-    
-    
-    answer = bob * sob
-            
-    
-    return answer
+    return bob * sob
