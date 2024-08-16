c24256e50c81edf45bb43c5d1f4d928a2b317460
Mo <91646814+Mo-bile@users.noreply.github.com>
2024-08-16 15:34:33 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,25 +1,22 @@
-def solution(wallpaper):
-    answer = []
-    
-    top, down,left, right = -2, 0, 50, 0
+def solution(wallpaper):    
+    top, down,left, right = None, None, float('inf'), -float('inf')
     
     for i in range(len(wallpaper)):
         row = wallpaper[i]        
 
         loc = row.find('#')
         rloc = row.rfind('#')
-        print(loc, rloc)
-        if loc != -1 or rloc != -1:
-            if top == -2 : 
+
+        if loc != -1: # find 성공했을 때 
+            if top == None : # top 을 아직 찾은적이 없을 때
                 top = i
-            if top != -2 :
-                down = i
+            down = i
 
             left = min(left, loc)
             right = max(right, rloc)
 
-    answer.append(top)
-    answer.append(left)
-    answer.append(down + 1)
-    answer.append(right + 1)
-    return answer
+    # answer.append(top)
+    # answer.append(left)
+    # answer.append(down + 1)
+    # answer.append(right + 1)
+    return [top, left, down + 1, right + 1]
