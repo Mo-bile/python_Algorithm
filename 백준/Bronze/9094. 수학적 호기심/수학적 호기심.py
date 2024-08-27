44c32c1526f62649a1e55f6322e336856c685eef
Mo <91646814+Mo-bile@users.noreply.github.com>
2024-08-27 19:41:17 +0900

    [Bronze III] Title: 수학적 호기심, Time: 3796 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -1,19 +1,17 @@
+from itertools import combinations
+
 if __name__ == "__main__":
 
     case_number = int(input())
-
-    case = []
-    for _ in range(case_number):
-        case.append(list(map(int,input().split())))
-
-    def numerator(a, b, m):
-        return a**2 + b**2 + m # 계속 ^ 이걸로 쉬프트연산으로 함
-
-
-    for n, m in case:
+    def count_valid_pairs(n, m):
         count = 0
-        for a in range(1,n):
-            for b in range(a + 1, n):
-                if numerator(a,b,m) % (a*b) == 0:
-                    count += 1
+        for combo in combinations(range(1,n), 2):
+            numerator = combo[0]**2 + combo[1]**2 + m
+            mother = (combo[0] * combo[1])
+            if numerator % mother == 0:
+                count += 1
         print(count)
+
+    for _ in range(case_number):
+        l = list(map(int, input().split()))
+        count_valid_pairs(l[0], l[1])
