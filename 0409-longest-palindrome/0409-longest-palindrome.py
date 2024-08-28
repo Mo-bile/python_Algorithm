7b5fee773e853893a3ea131e69112378aa3fa663
Mo <91646814+Mo-bile@users.noreply.github.com>
2024-08-28 22:36:32 +0900

    Time: 36 ms (62.57%), Space: 16.6 MB (19.12%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,50 @@
+# from collections import deque
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        s_hash = {}
+
+        for c in s:
+            if c not in s_hash:
+                s_hash[c] = 1
+            else :
+                s_hash[c] += 1
+        print(s_hash)
+        odd_found = False
+        # s_hash = dict(sorted(s_hash.items(), key=lambda item: item[1]))
+
+        count = 0
+        odd_found = False
+
+        for value in s_hash.values():
+            if value % 2 == 0:
+                count += value  # 짝수 개수는 그대로 추가
+            else:
+                count += value - 1  # 홀수 개수에서 1을 빼고 추가
+                odd_found = True  # 홀수가 있음을 표시
+
+        if odd_found:
+            count += 1  # 홀수가 하나라도 있으면 중앙에 하나를 추가 가능
+        return count
+
+# 직접 값을 입력하는 방식
+        # string = deque()
+        # sorted_hash = dict(sorted(s_hash.items(), key=lambda item: item[1]))
+        # print(sorted_hash)
+
+        # if len(s_hash) == 1:
+        #     for _ in range(sorted_hash[s[0]]):
+        #         string.append(s[0])
+        #     return len(string)
+
+        # for key, value in sorted_hash.items():
+        #     if value == 1 and not string :
+        #         string.append(key)
+        #     elif value != 1:
+        #         for i in range(0, value, 2):
+        #             print(i)
+
+        #             string.appendleft(key)
+        #             string.append(key)
+        # print(string)
+
+        # return len(string)
