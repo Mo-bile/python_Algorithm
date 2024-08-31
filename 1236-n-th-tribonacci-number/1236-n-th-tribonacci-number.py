6982a6846aa3ad6b0e490b46b4c997346c045c4b
Mo <91646814+Mo-bile@users.noreply.github.com>
2024-08-31 15:09:58 +0900

    Time: 33 ms (66.59%), Space: 16.5 MB (16.31%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -1,18 +1,16 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         t = [0 for _ in range(n + 1)]
-        if n < 1:
-            return t[n]
+        if n < 1: return t[n]
+        
         t[1] = 1
-        if n < 2:
-            return t[n]
+        if n < 2: return t[n]
+
         t[2] = 1
-        if n < 3:
-            return t[n]
+        if n < 3: return t[n]
 
         i = 3
         while i < n + 1:
             t[i] = t[i-3] + t[i-2] + t[i-1]
             i += 1
-
         return t[-1]
