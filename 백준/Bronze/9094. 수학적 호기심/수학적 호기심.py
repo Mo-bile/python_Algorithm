68eef3381e96cb2868103fbe0bdf4a3dc0cebeba
Mo <91646814+Mo-bile@users.noreply.github.com>
2024-09-01 11:01:17 +0900

    [Bronze III] Title: 수학적 호기심, Time: 3160 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -1,17 +1,18 @@
-from itertools import combinations
+import sys
+from  itertools import combinations
 
-if __name__ == "__main__":
+def integer_count(n,m):
+    count = 0
+    for combo in combinations(range(1, n),2):
+        if (combo[0]**2 + combo[1]**2 + m) % (combo[0]*combo[1]) == 0:
+            count += 1
+    return count
 
-    case_number = int(input())
-    def count_valid_pairs(n, m):
-        count = 0
-        for combo in combinations(range(1,n), 2):
-            numerator = combo[0]**2 + combo[1]**2 + m
-            mother = (combo[0] * combo[1])
-            if numerator % mother == 0:
-                count += 1
-        print(count)
 
-    for _ in range(case_number):
-        l = list(map(int, input().split()))
-        count_valid_pairs(l[0], l[1])
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    n, m = map(int,sys.stdin.readline().split())
+    count = integer_count(n, m)
+    print(count)
+
