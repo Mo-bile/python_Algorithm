990384544dc4f8fb3ac33f0dbf3ee22c5cf9a367
Mo <91646814+Mo-bile@users.noreply.github.com>
2024-11-12 10:32:22 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 57.70 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,15 +1,8 @@
 def solution(arr):
     answer = []
-    
+    answer.append(arr[0])
     for a in arr:
-        if len(answer) == 0:
+        if answer[-1] != a:
             answer.append(a)
-        else :
-            if answer[-1] != a:
-                answer.append(a)
-            
+    
     return answer
-        
-# set()은 안됨 -> [1,3,0,1] 중 [0,1,3] 으로 나옴
-# hash() 도 안됨 -> 마찬가지
-# 즉 두개 다 중복이 없고, 순서가 정렬되어서 출력됨
