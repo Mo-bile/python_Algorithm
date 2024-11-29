8fdf982a571bb8970bdd1fbac94f549f16c524f4
Mo <91646814+Mo-bile@users.noreply.github.com>
2024-11-29 09:47:57 +0900

    [level 2] Title: H-Index, Time: 0.18 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,19 +1,14 @@
-def solution(citations):
-    citations.sort(reverse=True)
-    
-#     for citation in citations:
-#         h = 0
-#         for v in citations:
-#             if citation >= v:
-#                 h += 1
-                
-#         if h == citation:
-#             return citation
+# key 해당 value 보다 뒤에 남은 인자의 갯수비교
 
-    for i in range(len(citations)):
-        # rank = i + 1
-        
-        if i >= citations[i]:
-            return i
-        
-    return len(citations)
+def solution(citations):       
+    # 1. 정렬
+    citations.sort(reverse = True)
+    h = 0
+    # 2. 현재 citation 의 값과 자기 포함 뒤에 남은 것보다 큰지 비교
+    for i, c in enumerate(citations):
+        # 2-1 크다면 answer 에 담고, 다음인덱스로 이동
+        if c >= i + 1:
+            h = i + 1
+        else :
+            break
+    return h
